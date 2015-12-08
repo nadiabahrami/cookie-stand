0a1cff2801ca530033939b6a98ae3a4a5db4fa95
Nadia Bahrami <nadia.bahrami@gmail.com>
2015-12-08 10:41:23 -0800

    Constructor objects made

--- app.js
+++ app.js
@@ -1,106 +1,48 @@
 var hours =["10am: ","11am: ", "12pm: ","1pm: ","2pm: ", "3pm: ","4pm: ", "5pm: ", "Total: "];
 
-var pike = {
-  min:17,
-  max:88,
-  aveCookies: 5.2,
-  total:0,
-  custRandom: function(){
-    return Math.round(Math.random()*(this.max-this.min)+ this.min);
-  },
-    cHours:[],
-  hourly: function(){
-    for(i=0; i<hours.length-1;i++){
-      this.cHours[i] = (Math.ceil(this.custRandom()*this.aveCookies));
-      this.total+=this.cHours[i];
-    };
-    this.cHours.push(this.total);
-  }
+
+function shop(min,max,aveCookies) {
+  this.min = min;
+  this.max = max;
+  this.aveCookies = aveCookies;
+}
+
+shop.prototype.custRandom = function () {
+  return Math.round(Math.random()*(this.max-this.min)+ this.min);
 };
 
- function lists(id,location){
-   var store = document.getElementById(id);
-   var createUl = document.createElement("ul");
-   createUl.appendChild(document.createTextNode('Hours'));
-   for(i=0;i<hours.length;i++){
-     var createLi = document.createElement("li");
-     createLi.textContent = hours[i]+location.cHours[i]+" cookies";
-     createUl.appendChild(createLi);
-   };
-   store.appendChild(createUl);
- }
-
-var seatac = {
-  min:6,
-  max:44,
-  aveCookies: 1.2,
-  total:0,
-  custRandom: function(){
-    return Math.round(Math.random()*(this.max-this.min)+ this.min);
-  },
-    cHours:[],
-  hourly: function(){
-    for(i=0; i<hours.length-1;i++){
-      this.cHours[i] = (Math.ceil(this.custRandom()*this.aveCookies));
-      this.total+=this.cHours[i];
-    };
-    this.cHours.push(this.total);
-  }
+shop.prototype.hourly = function () {
+  this.total = 0;
+  this.cHours=[];
+  for(i=0; i<hours.length-1;i++){
+    this.cHours[i] = (Math.ceil(this.custRandom()*this.aveCookies));
+    this.total+=this.cHours[i];
+    console.log(this.total);
+  };
+  this.cHours.push(this.total);
+  console.log(this.cHours);
 };
 
-var scMall = {
-  min:11,
-  max:38,
-  aveCookies: 1.9,
-  total:0,
-  custRandom: function(){
-    return Math.round(Math.random()*(this.max-this.min)+ this.min);
-  },
-    cHours:[],
-  hourly: function(){
-    for(i=0; i<hours.length-1;i++){
-      this.cHours[i] = (Math.ceil(this.custRandom()*this.aveCookies));
-      this.total+=this.cHours[i];
-    };
-    this.cHours.push(this.total);
-  }
-};
+var pike = new shop(17,88,5.2);
+var seatac = new shop(6,44,1.2);
+var scMall = new shop(11,38,1.9);
+var bellsq = new shop(20,48,3.3);
+var alki = new shop(3,24,2.6);
 
-var bellsq = {
-  min:20,
-  max:48,
-  aveCookies: 3.3,
-  total:0,
-  custRandom: function(){
-    return Math.round(Math.random()*(this.max-this.min)+ this.min);
-  },
-    cHours:[],
-  hourly: function(){
-    for(i=0; i<hours.length-1;i++){
-      this.cHours[i] = (Math.ceil(this.custRandom()*this.aveCookies));
-      this.total+=this.cHours[i];
-    };
-    this.cHours.push(this.total);
-  }
-};
-
-var alki = {
-  min:3,
-  max:24,
-  aveCookies: 2.6,
-  total:0,
-  custRandom: function(){
-    return Math.round(Math.random()*(this.max-this.min)+ this.min);
-  },
-    cHours:[],
-  hourly: function(){
-    for(i=0; i<hours.length-1;i++){
-      this.cHours[i] = (Math.ceil(this.custRandom()*this.aveCookies));
-      this.total+=this.cHours[i];
-    };
-    this.cHours.push(this.total);
-  }
-};
+function lists(id,location){
+ var store = document.getElementById(id);
+ var createTr = document.createElement("tr");
+ //createTr.appendChild(document.createTextNode('Hours'));
+ for(i=0;i<hours.length;i++){
+   var createTd1 = document.createElement("td");
+   var createTd2 = document.createElement("td");
+   createTd1.textContent = hours[i]
+   createTd2.textContent = location.cHours[i]+" cookies";
+   createTr.appendChild(createTd1);
+   createTr.appendChild(createTd2);
+ };
+ store.appendChild(createTr);
+}
 
 pike.hourly();
 lists("pikey",pike);
